7ba141994071b1561df2782369a55711f51e32e3
chaeyeon-yang <myday0827@naver.com>
2024-04-17 00:59:05 +0900

    ✨  [feat]: template method 구현

---
+++ src/main/java/com/hana/app/frame/HanaRepository.java
@@ -0,0 +1,14 @@
+package com.hana.app.frame;
+
+import java.util.List;
+
+public interface HanaRepository<K,V> {
+
+    int insert(V v) throws Exception;
+    int delete(K k) throws Exception;
+    int update(V v) throws Exception;
+    V selectOne(K k) throws Exception;
+    List<V> select() throws Exception;
+    List<V> selectAddr(String k);
+
+}
