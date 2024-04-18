e8f5cf81163dc20a4216fef198c88d3cbf6871f6
chaeyeon-yang <myday0827@naver.com>
2024-04-19 01:11:01 +0900

    ✨ [api]: 공지사항 dto, repo, service, mapper 세팅(#19)

---
+++ src/main/java/com/hana/app/service/NoticeService.java
@@ -0,0 +1,40 @@
+package com.hana.app.service;
+
+import com.hana.app.data.dto.NoticeDto;
+import com.hana.app.frame.HanaService;
+import com.hana.app.repository.NoticeRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class NoticeService implements HanaService<Integer, NoticeDto> {
+    final NoticeRepository noticeRepository;
+
+    @Override
+    public int add(NoticeDto noticeDto) throws Exception {
+        return 0;
+    }
+
+    @Override
+    public int del(Integer integer) throws Exception {
+        return 0;
+    }
+
+    @Override
+    public int modify(NoticeDto noticeDto) throws Exception {
+        return 0;
+    }
+
+    @Override
+    public NoticeDto get(Integer integer) throws Exception {
+        return null;
+    }
+
+    @Override
+    public List<NoticeDto> get() throws Exception {
+        return noticeRepository.select();
+    }
+}
