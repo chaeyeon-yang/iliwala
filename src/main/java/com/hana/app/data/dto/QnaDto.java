33426f0b396997a70ddd3c466e6ad2e337bdc5fc
chaeyeon-yang <myday0827@naver.com>
2024-04-20 01:36:58 +0900

    📄 [docs]: 일명 변경(#27)

--- src/main/java/com/hana/app/data/dto/QnaDto.java
+++ src/main/java/com/hana/app/data/dto/QnaDto.java
@@ -1,2 +1,21 @@
-package com.hana.app.data.dto;public class QnaDto {
+package com.hana.app.data.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.sql.Date;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class QnaDto {
+    private int qnaIdx;
+    private String qnaName;
+    private String qnaPw;
+    private String qnaTitle;
+    private String qnaContent;
+    private Date qnaDate;
 }
