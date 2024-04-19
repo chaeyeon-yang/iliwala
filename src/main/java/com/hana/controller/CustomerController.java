6b980464ae27665fbbea405c91dc3247b633d6f2
chaeyeon-yang <myday0827@naver.com>
2024-04-20 01:34:35 +0900

    📄 [docs]: controller 파일명 변경#(#27)

---
+++ src/main/java/com/hana/controller/CustomerController.java
@@ -0,0 +1,43 @@
+package com.hana.controller;
+
+import com.hana.app.data.dto.One2oneDto;
+import com.hana.app.data.dto.QnaDto;
+import com.hana.app.service.One2OneService;
+import com.hana.app.service.QnaService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("/customer")
+@RequiredArgsConstructor
+public class CustomerController {
+    String dir = "customer/";
+
+    final One2OneService one2OneService;
+
+    final QnaService qnaService;
+
+    @RequestMapping("/inquiry")
+    public String inquiry(Model model) throws Exception {
+        model.addAttribute("center",dir+"inquiry");
+        return "index";
+    }
+
+    @RequestMapping("/inquiryimpl")
+    public String inquiryimpl(One2oneDto one2oneDto) throws Exception {
+        one2OneService.add(one2oneDto);
+        return "index";
+    }
+
+    @RequestMapping("/askanswer")
+    public String askanswer(Model model) throws Exception {
+        List<QnaDto> qnaList = qnaService.get();
+        model.addAttribute("qnaList",qnaList);
+        model.addAttribute("center",dir+"askanswer");
+        return "index";
+    }
+}
